4d432601a525d65510049744c00e03de11a447b5
Geetanjali <gkalra88@gmail.com>
2025-06-12 16:28:51 +0400

    factortialProgram

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+	int num=4;
+	int factorial=1;
+	
+	for(int i=num;i>=1;i--) {
+		factorial=factorial*i;
+		
+	}
+	System.out.println(factorial);
+}}
